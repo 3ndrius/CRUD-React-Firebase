dae0f2cbbaba167f054687c37c9bb7a219581672
3ndrius <andrzejo1991@gmail.com>
2018-07-24 15:17:09 +0200

    Implement adding item to firebase and get item from database

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+
+import firebase from 'firebase'
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1r8zhG6ev97YcjKizW89eFw-rQCEVLYw",
+    authDomain: "react-8144f.firebaseapp.com",
+    databaseURL: "https://react-8144f.firebaseio.com",
+    projectId: "react-8144f",
+    storageBucket: "react-8144f.appspot.com",
+    messagingSenderId: "958629828580"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
